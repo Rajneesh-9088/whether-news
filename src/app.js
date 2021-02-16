515d3dbd9ff826c467fe65474cd39f2192ba3916
proraj9088 <rp8735685@gmail.com>
2021-02-16 23:59:14 +0530

    static page created
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express =   require('express') 
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 3000
 
 
-
+app.use(express.static(path.join(__dirname, "../public")))
+// routing
 app.get("/", (req,res) =>{
-    res.send("Welcome to your Entry")
+    res.send("Welcome to you in home page")
 })
 
 app.get("/about", (req,res) =>{
-    res.send("Welcome to your Entry")
+    res.send("Welcome to you in about page")
+})
+app.get("/whether", (req,res) =>{
+    res.send("Welcome to you in whether page")
+    
+})
+app.get("*", (req,res) =>{
+    res.send("404 error page oops")
+    // res.status(404)
 })
 
 
